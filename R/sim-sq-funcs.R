70f046ea80078c030ff5140c7ce32a5063707140
kendall@bren.ucsb.edu <kendall@bren.ucsb.edu>
2013-08-19 11:51:51 -0700

    Add documentation for coverage.unif

--- R/sim-sq-funcs.R
+++ R/sim-sq-funcs.R
@@ -95,6 +95,23 @@ coverage.rand = function(res.num, range.x, PR.x, c1 = runif(2, 1, PR.x),
   }
   return(frac)
 }
+#' Calculate the proportion of a square species range that is covered by a
+#' uniform reserve network
+#' 
+#' @param x.unif the length of one side a of a reserve
+#' @param rep.unif the linear dimension of a reserve+gap unit
+#' @param n.unif unused parameter(!)
+#' @param range.x length of one side of the species range
+#' @param c1 length-2 vector giving the lower left coordinate of the species 
+#'    range. Defaults to a random location in the planning region
+#' @param c2 length-2 vector giving the upper right coordinate of the species
+#'    range. Defaults to a value that will produce a square range with side
+#'    range.x. \textbf{This should never be set to a non-default value!}
+#' @return fraction of the species range that is included in reserves
+#' @details The ability
+#'    to specify `c2` suggests that I intended this to be used with rectangular
+#'    species ranges; but the code explicitly assumes that the range has
+#'    length `range.x` in both dimensions.
 
 coverage.unif = function(x.unif, rep.unif, n.unif, range.x,
                          c1 = sort(runif(2, 0, rep.unif)), c2 = c1 + range.x) {
